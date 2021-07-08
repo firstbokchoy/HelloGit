c68c568c5f8fd999aea3ca5b3214a10cb0739b7f
bjl <1256678327@qq.com>
2021-07-08 14:04:25 +0800

    克隆添加

--- HelloGit/src/main/java/com/bjl/git/HelloGit.java
+++ HelloGit/src/main/java/com/bjl/git/HelloGit.java
@@ -8,6 +8,7 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
 System.out.println("zai'xian'geng'xin");
+        System.out.println("克隆项目添加");
 
     }
 }
